cc192abc0745c6c94aec6c05997860264953cf7b
Ajay Patidar <ajaypatidar2112@gmail.com>
2022-02-15 14:02:19 +0530

    Second Commit

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
-const bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
 
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
 app.get('/', function(req, res){
-    res.send("Hello welcome to the website");
+    res.render("home");
 })
+
+app.get('/templets' ,function(req, res){
+    res.render("templets");
+});
 
 app.listen('3000', function(){
     console.log('listening on http://localhost:3000');
